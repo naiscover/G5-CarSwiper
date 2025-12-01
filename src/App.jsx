6e4337da121b286fb386df7f1ddc9d4f8eb97cdc
nai <ivantieu109@gmail.com>
2025-12-01 00:09:57 -0500

    Rename title from 'Finder' to 'Swiper'

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
     <div className="app">
       <div className="top-section">
         <h1 className="title">
-          Car<span>Finder</span>
+          Car<span>Swiper</span>
         </h1>
         <img src={carImg} alt="Car" className="car-img" />
       </div>
